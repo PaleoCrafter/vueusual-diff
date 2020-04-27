7fdff721dd633ca6f2631828a2096ede8504cdf8
Marvin Rösch <marvinroesch99@gmail.com>
2020-04-27 22:11:51 +0200

    Expose flexible linear differ

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import LinearDiff from './LinearDiff'
 import TreeDiff from './TreeDiff'
+import LinearDiffer from './LinearDiffer'
 import { Differ as TreeDiffer, Tree, TreeNode } from './tree-diff'
-import LinearDiffer, { DIFF_DELETE, DIFF_EQUAL, DIFF_INSERT } from './linear-diff'
+import { DIFF_DELETE, DIFF_EQUAL, DIFF_INSERT } from './linear-diff'
 
 const Edits = { EQUAL: DIFF_EQUAL, INSERT: DIFF_INSERT, DELETE: DIFF_DELETE }
 
